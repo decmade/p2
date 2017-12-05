94a6b2a5d0feda789bb9c6e98eb61a0ec1814db5
John Wesley Brown, Jr <john.w.brown.jr@gmail.com>
2017-12-05 11:19:06 -0500

    tested Hibernate settings by persisting a test User

--- src/main/java/com/cloudgames/ConsoleLauncher.java
+++ src/main/java/com/cloudgames/ConsoleLauncher.java
@@ -1,6 +1,7 @@
 package com.cloudgames;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.cloudgames.dbal.HibernateSessionUtil;
 import com.cloudgames.entities.User;
@@ -8,11 +9,15 @@ import com.cloudgames.entities.User;
 public class ConsoleLauncher {
 	public static void main(String[] args) {
 		Session session = HibernateSessionUtil.getSession();
+		Transaction tx = session.beginTransaction();
 		
 		User user = new User();
 		user.setLastName("Brown");
 		user.setFirstName("John");
 		
 		session.persist(user);
+		
+		tx.commit();
+		session.close();
 	}
 }
