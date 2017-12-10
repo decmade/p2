cee580effd67ce8d3111325de49c0a2b716495d3
Brandon Richardson <brichardson1222@yahoo.com>
2017-12-09 21:45:02 -0500

    fixes for account, transaction, and transactiontype stacks

--- src/main/java/com/cloudgames/entities/Transaction.java
+++ src/main/java/com/cloudgames/entities/Transaction.java
@@ -9,6 +9,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.cloudgames.entities.interfaces.TransactionInterface;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="transactions")
@@ -16,14 +17,14 @@ public class Transaction extends AbstractEntity implements TransactionInterface 
 
 	@Column(unique = false, nullable = false, precision = 15, scale = 2)
 	private double amount;
-	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name="account_id")
 	private Account acc;
 	@Column
 	private static final int STATUS_PENDING = 1;
 	@Column
 	private static final int STATUS_COMPLETED = 2;
-	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name="transactionType_id")
 	private TransactionType transactionType;
 	
--- src/main/java/com/cloudgames/repositories/TransactionTypeRepository.java
+++ src/main/java/com/cloudgames/repositories/TransactionTypeRepository.java
@@ -1,8 +1,11 @@
 package com.cloudgames.repositories;
 
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.springframework.stereotype.Repository;
 
+import com.cloudgames.entities.TransactionType;
 import com.cloudgames.entities.interfaces.TransactionTypeInterface;
 import com.cloudgames.repositories.interfaces.TransactionTypeRepositoryInterface;
 
@@ -10,9 +13,40 @@ import com.cloudgames.repositories.interfaces.TransactionTypeRepositoryInterface
 public class TransactionTypeRepository extends AbstractHibernateRepository<TransactionTypeInterface> implements TransactionTypeRepositoryInterface  {
 
 	@Override
+	public TransactionTypeInterface fetchById(int id) {
+		log.debug("retrieving UserStatus with ID: " + id + " from persistent storage");
+		
+		return super.fetchById(id);
+	}
+
+	@Override
+	public List<TransactionTypeInterface> fetchAll() {
+		log.debug("retrieving all TransactionTypes from persistent storage");
+		
+		return super.fetchAll();
+	}
+
+	@Override
+	public TransactionTypeInterface save(TransactionTypeInterface type) {	
+		if ( type.getId() > 0 ) {
+			log.debug("updating TransactionType with ID: "  + type.getId() + " in persistent storage");
+		} else {
+			log.debug("added new TransactionType to persistent storage");
+		}
+		
+		return super.save(type);
+	}
+
+	@Override
+	public void delete(TransactionTypeInterface type) {
+		log.debug(("deleting TransactionType with ID: " + type.getId() ));
+		
+		super.delete(type);
+	}
+	
+	@Override
 	protected Criteria getCriteria() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.getSession().createCriteria(TransactionType.class);
 	}
 
 }
