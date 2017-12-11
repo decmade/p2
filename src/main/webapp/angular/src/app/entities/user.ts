269a3447dd39bcaf60ad348f84e063e41d0f69c8
rlcst926 <rlcst926@mail.rmu.edu>
2017-12-11 17:03:20 -0500

    cleaning up rob's copy of the branch

--- src/main/webapp/angular/src/app/entities/user.ts
+++ src/main/webapp/angular/src/app/entities/user.ts
@@ -1,3 +1,22 @@
+import { UserStatus } from './UserStatus';
+import { UserRole } from './UserRole';
+
 export class User {
-    
+    id: Number;
+    identity: String;
+    credential: String;
+    secret: String;
+    lastName: String;
+    firstName: String;
+    email: String;
+    address1: String;
+    address2: String;
+    city: String;
+    state: String;
+    zip: String;
+    dob: Date;
+    phone: String;
+    account: Account;
+    status: UserStatus;
+    role: UserRole;
 }
