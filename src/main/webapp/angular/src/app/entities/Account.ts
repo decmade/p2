00e982b29e89bbd953b4bf860f1b144846009601
Brandon Richardson <brichardson1222@yahoo.com>
2017-12-11 13:09:25 -0500

    added entities into angular

---
+++ src/main/webapp/angular/src/app/entities/Account.ts
@@ -0,0 +1,4 @@
+export class Account {
+    id: Number;
+    balance: Number;
+  }
---
+++ src/main/webapp/angular/src/app/entities/Venue.ts
@@ -0,0 +1,11 @@
+export class Venue {
+    id: Number;
+    name: String;
+    city: String;
+    state: String;
+    zip: String;
+    address: String;
+    capacity: Number;
+    surface: String;
+    roofType: String;
+}
